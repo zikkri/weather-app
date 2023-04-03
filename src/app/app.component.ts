b07b040623524786dd96f05696b73d8351b9047b
zack <98119870+zikkri@users.noreply.github.com>
2023-04-03 18:17:25 -0500

    fucking about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
