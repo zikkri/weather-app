5505d96764c2020f8374e90f6b5961ea38d6bfdd
zack <98119870+zikkri@users.noreply.github.com>
2023-04-03 17:32:52 -0500

    Update weather.service.ts
    may be on the right track, may not be

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'TESTING';
+const API_URL = 'https://api.weatherstack.com';
+const API_KEY = '0672ee8263d1e089b2ded819150e2f6f';
+
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +13,8 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) {}
 
   //this is where we will call the api and get the data to pass to frontend
-  // getWeather() : Observable<Array<string>> {
-  //   return this.httpClient.get
-  // }
+  //think this needs to have its own class
+  getCurrentWeather(city: string) : Observable<Array<string>> {
+    return this.httpClient.get<Array<string>>(`${API_URL}/current?${API_KEY}&${city}`)
+  }
 }
